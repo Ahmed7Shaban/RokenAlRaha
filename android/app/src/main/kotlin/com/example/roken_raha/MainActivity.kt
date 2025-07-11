84c18c151761ee29fb6d617777de9ee5b08659d8
Ahmed Shaban <ahmed791shaban@gmail.com>
2025-07-12 01:53:54 +0300

    initialize Roken Al Raha project

---
+++ android/app/src/main/kotlin/com/example/roken_raha/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.roken_raha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar', 'en'),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(S.of(context).appTitle)),
+      body: Center(
+        child: Text(
+          S.of(context).homePageTitle,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
